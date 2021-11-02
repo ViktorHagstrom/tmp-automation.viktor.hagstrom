712891787f672c63af24df17f8d3105c550aa7a6
Viktor Hagtrom <viktor.hagstrom@hotmail.se>
2021-11-02 10:24:41 +0100

    started working on dashboard PO and played around with before/afterEach

--- frontend-cypress/cypress/integration/regression-test-suite.spec.js
+++ frontend-cypress/cypress/integration/regression-test-suite.spec.js
@@ -1,13 +1,30 @@
 /// <reference types="cypress" />
 
 import * as indexFuncs from '../pages/indexPage'
+import * as dashFuncs from '../pages/dashboardPage'
 
 describe ('Regression test suite', function(){
 
-it('Is a test', function(){
+    beforeEach(function(){
 
-    indexFuncs.login(cy)
+        indexFuncs.login(cy)
+        dashFuncs.dashCheckTitle(cy)
+        dashFuncs.dashAssertUser(cy)
+    });
+    afterEach(function(){
+
+        dashFuncs.dashLogOut(cy)
+        indexFuncs.indexCheckTitle(cy)
+    });
+
+    
+
+it('T2: Create room', function(){
+ 
+    
 })
+
+
 
 
 })
---
+++ frontend-cypress/cypress/pages/dashboardPage.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+//Elements
+const userField = '.user'
+const user = 'Welcome tester01!' // CHANGE USER HERE
+const logOutBtn = '#app > header > div > div > button'
+const dashboardTitle = 'Testers Hotel'
+const roomsView = '#app > div > div > div:nth-child(1) > a'
+const clientsView = '#app > div > div > div:nth-child(2) > a'
+const billsView = '#app > div > div > div:nth-child(3) > a'
+const reservationsView = '#app > div > div > div:nth-child(4) > a'
+
+//Actions
+function dashCheckTitle(cy){
+    cy.title().should('eq',dashboardTitle)
+}
+function dashAssertUser(cy){
+cy.get(userField).contains(user)
+}
+function dashLogOut(cy){
+    cy.get(logOutBtn).click()
+}
+
+//Export
+
+module.exports ={
+dashCheckTitle,
+dashAssertUser,
+dashLogOut
+
+}
