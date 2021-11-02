f45732e294582e2dd29be36b8dd272862bdb8885
Viktor Hagtrom <viktor.hagstrom@hotmail.se>
2021-11-02 14:48:20 +0100

    Worked on createRoomPage

--- frontend-cypress/cypress/pages/createRoomPage.js
+++ frontend-cypress/cypress/pages/createRoomPage.js
@@ -9,16 +9,19 @@ const numberField =':nth-child(2) > input'
 const floorField=':nth-child(3) > input'
 const avaliableBox='.checkbox'
 const priceField=':nth-child(5) > input'
-const featuresField=':nth-child(5) > input'
-const featBalcony ='[value="balcony"]'
-const featEnsuite='[value="ensuite"]'
-const featSeaView='[value="sea_view"]'
-const featPenthouse='[value="penthouse"]'
+const featuresField=':nth-child(6) > select'
+const featBalcony ='Balcony'
+const featEnsuite='Ensuite'
+const featSeaView='Sea View'
+const featPenthouse='Penthouse'
 const backBtn='[href="/rooms"]'
-const savebtn='blue'
+const saveBtn='.blue'
+const featuresArray=[featBalcony,featEnsuite,featSeaView,featPenthouse]
     //Inputs
 const price ='2500'
 const category='Double'
+const roomNumber='103'
+const floorNumber='1'
 //Actions
 
 function assertNewRoom(cy){
@@ -27,6 +30,12 @@ function assertNewRoom(cy){
 }
 function createRoom(cy){
     cy.get(categoryField).select(category)
+    cy.get(numberField).type(roomNumber)
+    cy.get(floorField).type(floorNumber)
+    cy.get(avaliableBox).click()
+    cy.get(priceField).type(price)
+    cy.get(featuresField).select(featuresArray)
+    cy.get(saveBtn).click()
 }
 //Export
 module.exports = {
